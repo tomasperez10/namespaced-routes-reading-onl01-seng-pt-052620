18d5dffeb7d51a16bfcdd41f0cf264b066fa4354
Tomas Perez <tomasitopg6@gmail.com>
2020-10-26 17:06:04 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -5,9 +5,10 @@ Rails.application.routes.draw do
 
   resources :posts, only: %i[index show new create edit update]
 
-  scope '/admin', module: 'admin' do
-  resources :stats, only: [:index]
-end
+  namespace :admin do
+    resources :stats, only: [:index]
+  end
+
 
   root 'posts#index'
 end
